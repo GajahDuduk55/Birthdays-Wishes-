e32746883ee8c9863c6e7780274b55d7b28ee152
GajahDuduk55 <nabilaiman915@gmail.com>
2025-02-07 23:51:00 +0800

    main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('Happy Birthday Nienie Sayang<3').split('')
+    const titles = ('Happy Birthday Nienie<3').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
